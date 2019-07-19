7511f7305de7d0d0a54b2bda9f3003aad05673af
Jason Viola <jason.viola@mavs.uta.edu>
2019-07-19 21:10:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+  if speed > 60
+    p true
+  elsif speed < 40
+    p true
+  else
+    p false
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+speed > 60 ?  true : false
+speed < 40 ?  true : false
 end
-	
+
 
 
